01b225b771eab5a1f1301b8116c17321cc7c3bcd
coma <eduardo@comakai.com>
2018-06-08 07:31:45 +0200

    one more pdf file and some error handling...

--- app/src/main/java/com/worktoolslleida/app/PDFActivity.java
+++ app/src/main/java/com/worktoolslleida/app/PDFActivity.java
@@ -1,12 +1,15 @@
 package com.worktoolslleida.app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
 
-public class PDFActivity extends AppCompatActivity {
+public class PDFActivity extends AppCompatActivity implements OnPageErrorListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,19 @@ public class PDFActivity extends AppCompatActivity {
         PDFView pdfView = findViewById(R.id.pdfView);
         pdfView
                 .fromAsset(path)
+                .onPageError(this)
                 .load();
+    }
+
+    @Override
+    public void onPageError(int page, Throwable error) {
+        String message = "Vaya, he tenido un problema con ese pdf...";
+        Context context = getApplicationContext();
+
+        Toast
+                .makeText(context, message, Toast.LENGTH_SHORT)
+                .show();
+
+        finish();
     }
 }
